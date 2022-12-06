0ffe7a0b39ad5640860f5aaff7517bc97b229e0f
nrai14 <nishadrai14@gmail.com>
2022-12-06 19:46:08 +0000

    pseudo coding the overview of this project

---
+++ starter/script.js
@@ -0,0 +1,30 @@
+/* 
+Questions
+
+1) Inside which HTML element do we put the JavaScript? 
+<script>     <js>     <javascript>   <scripting>
+
+2) Where is the correct place to insert a JavaScript? 
+<body>       <head>             Both <head> and <body>
+
+3) Which event occurs when the user clicks on an HTML element? 
+onclick         onmouseover         onmouseclick            onchange 
+
+*/
+
+
+//Pseudo code 
+
+/* Opening page will have:
+
+Start button
+Instructions
+Leaderboard 
+
+When START is clicked = timer starts + first question
+    Answer chosen? Correct = sound --> next question. Wrong = sound --> timer down + next question
+        Continue until all questions answered or timer goes to 0
+    Results then shown with final score 
+        Option to write initials --> Submit on leaderboard --> also option to clear
+
+*/
